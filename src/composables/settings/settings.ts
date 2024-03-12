97688a753e0788eb2e52f2d481af73b80eee9c17
HenryWu1113 <bug910284@gmail.com>
2024-03-13 02:00:37 +0800

    套用圖片至首頁輪播圖

--- src/composables/settings/settings.ts
+++ src/composables/settings/settings.ts
@@ -11,3 +11,219 @@ export const menuSelection = [
     query: 'clothingGender=female&sort=integrate'
   }
 ]
+
+/** 首頁輪播圖控制 */
+export const homeCarousel = [
+  {
+    key: '1',
+    chlidren: [
+      {
+        key: '1-1',
+        src: new URL('../../assets/images/home/video1new.mp4', import.meta.url).href,
+        type: 'video',
+        info: {
+          title: '穿出自信，展現不凡',
+          text: '好衣服讓你自信，更加突出與眾不同',
+          price: null,
+          showBtn: true,
+          position: 'left'
+        }
+      },
+      {
+        key: '1-2',
+        src: new URL('../../assets/images/home/img1.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '風格獨特，品味出眾',
+          text: '獨特風格展現卓越品味，與眾不同',
+          price: null,
+          showBtn: true,
+          position: 'left'
+        }
+      },
+      {
+        key: '1-3',
+        src: new URL('../../assets/images/home/img2.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '時尚從這裡開始',
+          text: '在這裡找到你的時尚起點，引領潮流',
+          price: null,
+          showBtn: true,
+          position: 'left'
+        }
+      },
+      {
+        key: '1-4',
+        src: new URL('../../assets/images/home/img3.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '專屬你的時尚風格',
+          text: '找到屬於自己的風格，獨一無二',
+          price: null,
+          showBtn: true,
+          position: 'right'
+        }
+      }
+    ]
+  },
+  {
+    key: '2',
+    chlidren: [
+      {
+        key: '2-1',
+        src: new URL('../../assets/images/home/img4.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '簡約不凡，時尚永恆',
+          text: ' 簡約風格卻不失卓越，永不過時',
+          price: null,
+          showBtn: true,
+          position: 'right'
+        }
+      },
+      {
+        key: '2-2',
+        src: new URL('../../assets/images/home/img5.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '品味生活，從穿著開始',
+          text: '衣著展現品味，從這裡開始精彩生活',
+          price: null,
+          showBtn: true,
+          position: 'right'
+        }
+      },
+      {
+        key: '2-3',
+        src: new URL('../../assets/images/home/img6.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '穿出個性，展現魅力',
+          text: '衣服展現個性，散發自身獨特魅力',
+          price: null,
+          showBtn: true,
+          position: 'left'
+        }
+      },
+      {
+        key: '2-4',
+        src: new URL('../../assets/images/home/img7.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '讓衣服成為你的語言',
+          text: '用衣服表達自我，成為你的語言',
+          price: null,
+          showBtn: true,
+          position: 'left'
+        }
+      }
+    ]
+  },
+  {
+    key: '3',
+    chlidren: [
+      {
+        key: '3-1',
+        src: new URL('../../assets/images/home/img8.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '一件衣服，一段故事',
+          text: ' 每一件衣服背後都有一個故事，成就你的故事',
+          price: null,
+          showBtn: true,
+          position: 'right'
+        }
+      },
+      {
+        key: '3-2',
+        src: new URL('../../assets/images/home/img9.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '時尚風格，唯我獨尊',
+          text: '在這裡打造只屬於你的時尚風格',
+          price: null,
+          showBtn: true,
+          position: 'left'
+        }
+      },
+      {
+        key: '3-3',
+        src: new URL('../../assets/images/home/img10.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '時尚不僅是風格，更是態度',
+          text: '時尚不僅是外在表現，更是內在態度的展示',
+          price: null,
+          showBtn: true,
+          position: 'right'
+        }
+      },
+      {
+        key: '3-4',
+        src: new URL('../../assets/images/home/img11.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '經典再現，風格永恆',
+          text: '經典款式永不過時，展現永恆風格',
+          price: null,
+          showBtn: true,
+          position: 'right'
+        }
+      }
+    ]
+  },
+  {
+    key: '4',
+    chlidren: [
+      {
+        key: '4-1',
+        src: new URL('../../assets/images/home/img12.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '舒適至上，品味生活',
+          text: '追求舒適感，享受高品味生活',
+          price: null,
+          showBtn: true,
+          position: 'right'
+        }
+      },
+      {
+        key: '4-2',
+        src: new URL('../../assets/images/home/img13.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '穿上夢想，展現自我',
+          text: ' 每一件衣服都是夢想的一部分，穿上它們展現真正的自我',
+          price: null,
+          showBtn: true,
+          position: 'left'
+        }
+      },
+      {
+        key: '4-3',
+        src: new URL('../../assets/images/home/img14.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '風格無限，舞出青春',
+          text: '隨心所欲地展現風格，與青春共舞',
+          price: null,
+          showBtn: true,
+          position: 'left'
+        }
+      },
+      {
+        key: '4-4',
+        src: new URL('../../assets/images/home/img15.jpg', import.meta.url).href,
+        type: 'img',
+        info: {
+          title: '打造時尚典範，引領潮流',
+          text: '不僅展現時尚，更引領時尚潮流的發展',
+          price: null,
+          showBtn: true,
+          position: 'right'
+        }
+      }
+    ]
+  }
+]
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,57 @@
 <template>
-  <div ref="homePage" :class="['home-page', 'min-h-screen', swiperInstance?.isEnd ? 'showScrollBar' : '']"
-    @wheel="onscroll">
-    <swiper :direction="'vertical'" :slidesPerView="1" :mousewheel="true" :pagination="{
-    clickable: true
-  }" :modules="modules" class="mySwiper" ref="swiperDom" @reachEnd="handleSwiperEnd" @init="init">
-      <swiper-slide>Slide 1</swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
+  <div
+    ref="homePage"
+    :class="['home-page', 'min-h-screen', swiperInstance?.isEnd ? 'showScrollBar' : '']"
+    @wheel="onscroll"
+  >
+    <swiper
+      :direction="'vertical'"
+      :slidesPerView="1"
+      :mousewheel="true"
+      :pagination="{
+        clickable: true
+      }"
+      :modules="modules"
+      class="mySwiper"
+      :speed="800"
+      @init="init"
+    >
+      <swiper-slide v-for="section in homeCarousel" :key="section.key">
+        <swiper
+          :pagination="{
+            type: 'fraction'
+          }"
+          :loop="true"
+          :modules="modules"
+          class="mySwiper"
+          :speed="800"
+        >
+          <swiper-slide class="carousel-page" v-for="part in section.chlidren" :key="part.key">
+            <img class="carousel-img" v-if="part.type === 'img'" :src="part.src" />
+            <video
+              class="carousel-video"
+              v-if="part.type === 'video'"
+              :src="part.src"
+              loop
+              muted
+              autoplay
+            />
+            <div class="info-wrap" :style="infoPosition(part.info.position)">
+              <div class="title">
+                <n-gradient-text type="warning">
+                  {{ part.info.title }}
+                </n-gradient-text>
+              </div>
+              <div class="text" v-html="part.info.text"></div>
+              <div v-if="part.info.showBtn" class="see-more">瀏覽更多</div>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </swiper-slide>
     </swiper>
     <div></div>
   </div>
 </template>
-
-<style>
-/* body::-webkit-scrollbar {
-  display: none !important;
-} */
-</style>
 
 <style lang="scss" scoped>
 @import '@/styles/styles';
@@ -27,47 +62,81 @@
   .mySwiper {
     width: 100%;
     height: calc(100vh - 72px) !important;
-  }
 
-  .swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: black;
-    height: calc(100vh - 72px) !important;
-    color: white;
+    .swiper-slide {
+      text-align: center;
+      font-size: 18px;
+      background: black;
+      height: calc(100vh - 72px) !important;
+      color: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
 
-    /* Center slide text vertically */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+      &.carousel-page {
+        position: relative;
+        > img,
+        video {
+          display: block;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
 
-  .swiper-slide img {
-    display: block;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+        .info-wrap {
+          z-index: 1;
+          // width: 400px;
+          // height: 200px;
+          position: absolute;
+          top: 50%;
+          transform: translateY(-50%);
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          .title {
+            text-align: left;
+            font-size: 2.5rem;
+            font-weight: bold;
+            .n-gradient-text {
+              font-weight: bold;
+            }
+          }
+          .text {
+            text-align: left;
+            text-shadow: 2px 2px 5px black;
+          }
+          .see-more {
+            cursor: pointer;
+            margin-top: 1rem;
+            border: 1px solid white;
+            border-radius: 3px;
+            font-size: 1.5rem;
+            padding: 5px 1rem;
+            background: rgba(255, 255, 255, 0.5);
+            text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+            transition: 0.3s;
+            &:hover {
+              transform: translateY(-5px);
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { ref, reactive, onMounted, onBeforeUnmount } from 'vue'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Mousewheel, Pagination } from 'swiper/modules'
+import { homeCarousel } from '@/composables/settings/settings'
 import 'swiper/css/pagination'
 import 'swiper/css'
 const modules = [Mousewheel, Pagination]
 
 const homePage: any = ref(null)
 const swiperInstance: any = ref(null)
-
-// console.log(Swiper)
-
-const handleSwiperEnd = (value: any) => {
-  console.log('reach end')
-  // swiperInstance.value.mousewheel.disable()
-}
 
 const init = (swiper: any) => {
   console.log(swiper)
@@ -87,6 +156,11 @@ function onscroll(e: WheelEvent) {
     swiperInstance.value.mousewheel.enable()
     body.style.overflow = 'hidden'
   }
+}
+
+function infoPosition(position: string) {
+  if (position === 'left') return { left: '400px' }
+  if (position === 'right') return { right: '400px' }
 }
 
 onMounted(() => {
